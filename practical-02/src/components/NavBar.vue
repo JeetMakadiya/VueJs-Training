c55af43c67c860ed0c5148309e960de8bcc3bea0
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-29 14:30:40 +0530

    TE5-T637:Added new line in .gitignore and added data property in NavBar.vue file

--- practical-02/src/components/NavBar.vue
+++ practical-02/src/components/NavBar.vue
@@ -1,10 +1,15 @@
 <template>
-  <header class="bg-primary pt-2 pb-2">
-    <h1 class="text-center text-light">Car Showroom</h1>
-  </header>
+  <nav class="bg-primary pt-2 pb-2">
+    <h1 class="text-center text-light">{{ navBarText }}</h1>
+  </nav>
 </template>
 <script>
 export default {
   name: "NavBar",
+  data() {
+    return {
+      navBarText: "Car Showroom",
+    };
+  },
 };
 </script>
