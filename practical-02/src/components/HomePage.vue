8b2d6cb324a1c96c38d4f212ff053fa8d2143291
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-29 11:55:09 +0530

    TE5-T637:Practical-02 completed 90%

---
+++ practical-02/src/components/HomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <NavBar />
+    <div class="d-flex flex-wrap">
+      <GalleryCard
+        v-for="data in cardData"
+        :key="data.id"
+        :cardTitle="data.title"
+        :cardImage="data.image"
+        :cardDescription="data.description"
+        :price="data.price"
+        @showCarPrice="showCarPrice"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import GalleryCard from "./GalleryCard";
+import cardData from "../assets/Data/CardData.json";
+
+export default {
+  name: "HomePage",
+  components: {
+    GalleryCard,
+  },
+  data() {
+    return {
+      cardData: cardData,
+    };
+  },
+  methods: {
+    showCarPrice(price) {
+      alert(`Car Price : ${price}`);
+    },
+  },
+};
+</script>
--- practical-02/src/main.js
+++ practical-02/src/main.js
@@ -1,14 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
-
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import NavBar from "./components/NavBar.vue";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+
+// Creating global component: NavBar
+Vue.component("NavBar", NavBar);
 
 new Vue({
   render: (h) => h(App),
