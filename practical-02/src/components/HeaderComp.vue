9112d156e106925f599c1fe12f1b6449a8ccebd7
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-29 10:33:08 +0530

    TE5-T637: Initial Setup of Practical-02

---
+++ practical-02/src/components/HeaderComp.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="bg-primary pt-2 pb-2">
+    <h1 class="text-center text-light">Car Showroom</h1>
+  </header>
+</template>
+<script>
+export default {
+  name: "HeaderComp",
+};
+</script>
