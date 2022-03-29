1fef19f9246453bc60e15c55654d70d8d8c1ff9a
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-29 10:33:08 +0530

    TE5-T637: Initial Setup of Practical-02

---
+++ practical-02/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <HeaderComp />
+    <GalleryCard />
+  </div>
+</template>
+
+<script>
+import GalleryCard from "./components/GalleryCard";
+import HeaderComp from "./components/HeaderComp.vue";
+
+export default {
+  name: "App",
+  components: {
+    GalleryCard,
+    HeaderComp,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
