7dfedfeb5d138c21530b2a1761792221c8bed70e
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-30 14:31:17 +0530

    TE5-T637: Practical-03 Initial setup

---
+++ practical-03/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav
+    class="bg-primary d-flex justify-content-center align-items-center"
+    style="height: 14vh"
+  >
+    <h1 class="text-center text-light">{{ navBarText }}</h1>
+  </nav>
+</template>
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {
+      navBarText: "Car Showroom",
+    };
+  },
+};
+</script>
