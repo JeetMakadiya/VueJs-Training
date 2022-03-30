a0cb5168ab55e86096697edf14cf18a2eebef69e
JeetMakadiya <jeetmakadiya01@gmail.com>
2022-03-30 14:31:17 +0530

    TE5-T637: Practical-03 Initial setup

---
+++ practical-03/src/components/HomePage.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <NavBar />
+    <section style="height: calc(100vh - 14vh)">
+      <div class="pt-5 d-flex justify-content-center">
+        <div class="ms-2 me-2" v-for="data in cardData" :key="data.id">
+          <GalleryCard
+            :cardTitle="data.title"
+            :cardImage="data.image"
+            :cardDescription="data.description"
+            :price="data.price"
+            @showCarPrice="showCarPrice"
+          />
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import GalleryCard from "./GalleryCard";
+import cardData from "../assets/Data/CardData.json";
+
+export default {
+  name: "HomePage",
+  components: {
+    GalleryCard,
+  },
+  data() {
+    return {
+      cardData: cardData,
+    };
+  },
+  methods: {
+    showCarPrice(price) {
+      alert(`Car Price : ${price}`);
+    },
+  },
+};
+</script>
+
+<style>
+.galleryCardWrapper {
+  height: calc();
+}
+</style>
