dae0c3a1f988a1f22776ad0f8f3a4ab7b0116a5d
Jeet-Makadiya-Simform <jeet.m@simformsolutions.com>
2022-04-08 14:37:54 +0530

    TE5-T637: Initial Setup of Practical-04

---
+++ practical-04/src/components/GalleryCard.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- <div class="mt-1 mb-1 ms-1 me-1"> -->
+  <b-card
+    :title="cardTitle"
+    tag="article"
+    style="max-width: 20rem"
+    class="mb-2 position-relative"
+  >
+    <b-card-img :src="cardImage" alt="Image" height="150" bottom></b-card-img>
+    <b-card-text class="mb-5">
+      {{ cardDescription }}
+    </b-card-text>
+    <div class="position-absolute bottom-0 mb-1">
+      <!-- Info Button -->
+      <b-button
+        href="#"
+        variant="primary"
+        @click="$emit('showCarPrice', price)"
+        v-if="price !== null"
+      >
+        Info
+      </b-button>
+      <!-- available soon Button -->
+      <b-button href="#" variant="primary" disabled v-else>
+        available <br />soon
+      </b-button>
+      <!-- Edit Button -->
+      <b-button
+        href="#"
+        variant="primary"
+        class="ms-3"
+        v-b-modal="'modal-prevent-closing-' + cardId"
+        @click="$emit('editCard')"
+      >
+        Edit
+      </b-button>
+      <!-- Delete Button -->
+      <b-button
+        href="#"
+        variant="primary"
+        class="ms-3"
+        v-b-modal="'modal-prevent-closing-' + cardId"
+        @click="$emit('deleteCard')"
+      >
+        Delete
+      </b-button>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "GalleryCard",
+  components: {},
+  props: ["cardId", "cardTitle", "cardImage", "cardDescription", "price"],
+};
+</script>
