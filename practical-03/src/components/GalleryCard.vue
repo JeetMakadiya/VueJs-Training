b6086c6f5a4a7516fd1f7925e5f5224dccc47871
Jeet-Makadiya-Simform <jeet.m@simformsolutions.com>
2022-04-08 17:51:22 +0530

    TE5-T637: Make changes in card and button designs

--- practical-03/src/components/GalleryCard.vue
+++ practical-03/src/components/GalleryCard.vue
@@ -4,47 +4,65 @@
     :title="cardTitle"
     tag="article"
     style="max-width: 20rem"
-    class="mb-2 position-relative"
+    class="mb-2"
   >
     <b-card-img :src="cardImage" alt="Image" height="150" bottom></b-card-img>
     <b-card-text class="mb-5">
       {{ cardDescription }}
     </b-card-text>
-    <div class="position-absolute bottom-0 mb-1">
-      <!-- Info Button -->
-      <b-button
-        href="#"
-        variant="primary"
-        @click="$emit('showCarPrice', price)"
-        v-if="price !== null"
-      >
-        Info
-      </b-button>
-      <!-- available soon Button -->
-      <b-button href="#" variant="primary" disabled v-else>
-        available <br />soon
-      </b-button>
-      <!-- Edit Button -->
-      <b-button
-        href="#"
-        variant="primary"
-        class="ms-3"
-        v-b-modal="'modal-prevent-closing-' + cardId"
-        @click="$emit('editCard')"
-      >
-        Edit
-      </b-button>
-      <!-- Delete Button -->
-      <b-button
-        href="#"
-        variant="primary"
-        class="ms-3"
-        v-b-modal="'modal-prevent-closing-' + cardId"
-        @click="$emit('deleteCard')"
-      >
-        Delete
-      </b-button>
-    </div>
+    <template #footer>
+      <b-row>
+        <b-col cols="12">
+          <b-button
+            href="#"
+            variant="primary"
+            @click="$emit('showCarPrice', price)"
+            class="mb-2 w-100"
+            v-if="price !== null"
+          >
+            Info
+          </b-button>
+
+          <!-- available soon Button -->
+          <b-button
+            href="#"
+            variant="primary"
+            class="mb-2"
+            style="width: 100%"
+            disabled
+            v-else
+          >
+            available soon...
+          </b-button>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="6">
+          <!-- Edit Button -->
+          <b-button
+            href="#"
+            variant="primary"
+            class="w-100"
+            v-b-modal="'modal-prevent-closing-' + cardId"
+            @click="$emit('editCard')"
+          >
+            Edit
+          </b-button></b-col
+        >
+        <b-col cols="6">
+          <!-- Delete Button -->
+          <b-button
+            href="#"
+            variant="primary"
+            class="w-100"
+            v-b-modal="'modal-prevent-closing-' + cardId"
+            @click="$emit('deleteCard')"
+          >
+            Delete
+          </b-button>
+        </b-col>
+      </b-row>
+    </template>
   </b-card>
 </template>
 
--- practical-03/src/components/HomePage.vue
+++ practical-03/src/components/HomePage.vue
@@ -3,7 +3,7 @@
     <NavBar />
     <section>
       <b-container class="bv-example-row">
-        <b-row class="pt-5">
+        <b-row class="pt-5 justify-content-md-center">
           <b-col
             cols="3"
             class="d-flex align-items-stretch ms-2 me-2"
@@ -24,7 +24,10 @@
         </b-row>
       </b-container>
     </section>
-    <FormModal :formData="selectedCardData" />
+    <FormModal
+      :formData="selectedCardData"
+      @submittedFormData="handleSubmittedFormData"
+    />
   </div>
 </template>
 
@@ -71,7 +74,7 @@ export default {
       this.cardData = cardItems;
       alert("Deleted : " + data.title);
     },
-    handleSubmittedData(data) {
+    handleSubmittedFormData(data) {
       console.log(data);
       if (data.carId !== "") {
         // Edit Card
@@ -102,34 +105,34 @@ export default {
     },
   },
   mounted() {
-    this.$root.$on("submitted-form-data", (data) => {
-      if (data.carId !== "") {
-        // Edit Card
-        let id = data.carId;
-        let index = this.cardData.findIndex((item) => item.id === id);
-        this.cardData[index].title = data.carName;
-        this.cardData[index].image = data.carImgURL;
-        this.cardData[index].description = data.carDetails;
-        this.cardData[index].price = data.carPrice;
-        this.selectedCardData = {
-          carId: "",
-          carName: "",
-          carDetails: "",
-          carPrice: "",
-          carImgURL: "",
-        };
-      } else {
-        // Add New Card
-        let carDetails = {
-          id: new Date().getTime().toString(),
-          title: data.carName,
-          image: data.carImgURL,
-          description: data.carDetails,
-          price: data.carPrice,
-        };
-        this.cardData.push(carDetails);
-      }
-    });
+    // this.$root.$on("submitted-form-data", (data) => {
+    //   if (data.carId !== "") {
+    //     // Edit Card
+    //     let id = data.carId;
+    //     let index = this.cardData.findIndex((item) => item.id === id);
+    //     this.cardData[index].title = data.carName;
+    //     this.cardData[index].image = data.carImgURL;
+    //     this.cardData[index].description = data.carDetails;
+    //     this.cardData[index].price = data.carPrice;
+    //     this.selectedCardData = {
+    //       carId: "",
+    //       carName: "",
+    //       carDetails: "",
+    //       carPrice: "",
+    //       carImgURL: "",
+    //     };
+    //   } else {
+    //     // Add New Card
+    //     let carDetails = {
+    //       id: new Date().getTime().toString(),
+    //       title: data.carName,
+    //       image: data.carImgURL,
+    //       description: data.carDetails,
+    //       price: data.carPrice,
+    //     };
+    //     this.cardData.push(carDetails);
+    //   }
+    // });
   },
 };
 </script>
