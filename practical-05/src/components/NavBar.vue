d7ba7aa522b861a88c1d4893653abd6c32ae793e
Jeet-Makadiya-Simform <jeet.m@simformsolutions.com>
2022-04-19 18:00:59 +0530

    TE5-T637: router-link, router-view and api call in LoginForm added

--- practical-05/src/components/NavBar.vue
+++ practical-05/src/components/NavBar.vue
@@ -5,7 +5,7 @@
   >
     <h1 class="text-center text-light">{{ navBarText }}</h1>
     <div>
-      <router-link to="/" class="btn text-light fw-bold fs-5 nav_btn">
+      <router-link to="/" class="btn me-3 text-light fw-bold fs-5 nav_btn">
         Home
       </router-link>
       <router-link to="/login" class="btn me-3 text-light fw-bold fs-5 nav_btn">
@@ -17,19 +17,16 @@
       >
         Register
       </router-link>
-      <b-button variant="light" v-b-modal.modal-prevent-closing
-        >Add Car</b-button
-      >
+      <b-button variant="light" v-b-modal.modal-prevent-closing>
+        Add Car
+      </b-button>
     </div>
   </nav>
 </template>
 <script>
-// import FormModal from "./FormModal.vue";
 export default {
   name: "NavBar",
-  components: {
-    // FormModal,
-  },
+  components: {},
   data() {
     return {
       navBarText: "Car Showroom",
@@ -40,7 +37,11 @@ export default {
 
 <style>
 .nav_btn:hover {
-  background-color: rgba(255, 255, 255, 0.2);
   color: white !important;
+  background-color: rgba(255, 255, 255, 0.2);
+}
+.nav_btn.router-link-exact-active {
+  color: black !important;
+  background-color: white !important;
 }
 </style>
