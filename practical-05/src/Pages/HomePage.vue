8aaf979b8e17f372ce4bc1d56d10b6dd3a2593be
Jeet-Makadiya-Simform <jeet.m@simformsolutions.com>
2022-04-19 12:28:44 +0530

    TE5-T637: Routes added

--- practical-05/src/Pages/HomePage.vue
+++ practical-05/src/Pages/HomePage.vue
@@ -1,22 +1,22 @@
 <template>
   <div>
-    <NavBar />
-    <!-- <GalleryCardWrapper /> -->
-    <LoginPage />
+    <!-- <NavBar /> -->
+    <GalleryCardWrapper />
+    <!-- <LoginPage /> -->
     <!-- <RegisterPage /> -->
   </div>
 </template>
 
 <script>
-// import GalleryCardWrapper from "../components/GalleryCardWrapper.vue";
-import LoginPage from "./LoginPage.vue";
+import GalleryCardWrapper from "../components/GalleryCardWrapper.vue";
+// import LoginPage from "./LoginPage.vue";
 // import RegisterPage from "./RegisterPage.vue";
 
 export default {
   name: "HomePage",
   components: {
-    // GalleryCardWrapper,
-    LoginPage,
+    GalleryCardWrapper,
+    // LoginPage,
     // RegisterPage,
   },
 };
